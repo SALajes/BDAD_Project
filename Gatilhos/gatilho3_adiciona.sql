02f2dafd04f9a9d37ff76072dde670eb92e36e39
SALajes <lajesdoesthejob@gmail.com>
2019-05-26 16:53:50 +0100

    Gatilhos done

---
+++ Gatilhos/gatilho3_adiciona.sql
@@ -0,0 +1,6 @@
+Create Trigger DefineConvidado
+Before Insert On Convidado
+For Each Row
+When EXISTS(SELECT * FROM Normal WHERE participante = New.participante)
+Begin Select raise(abort, "Participante ja definido como Normal");
+End
---
+++ Gatilhos/gatilho3_verifica.sql
@@ -0,0 +1,2 @@
+Insert into Convidado values(11,3,15);
+Insert into Convidado values(130,2,8);
