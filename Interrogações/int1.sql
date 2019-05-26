c6fe245d63767ff664fa4e7c9c1afb082d0b947b
Pasgas99 <pasgas99@hotmail.com>
2019-05-26 18:37:45 +0100

    Added queries 1 and 10

--- Interrogações/int1.sql
+++ Interrogações/int1.sql
@@ -2,3 +2,10 @@
 .headers ON
 .nullvalue NULL
 
+SELECT Concertos.Dia, count(*) as NumPalcos
+FROM
+    (SELECT DISTINCT Concerto.data as Dia, Concerto.palco_num as PalcoNum, Concerto.palco_zona as PalcoZona
+    FROM Concerto) as Concertos
+GROUP BY Concertos.Dia
+HAVING (NumPalcos > (SELECT count(*)
+                    FROM Palco))
--- Interrogações/int10.sql
+++ Interrogações/int10.sql
@@ -2,3 +2,18 @@
 .headers ON
 .nullvalue NULL
 
+CREATE VIEW IF NOT EXISTS EncArtistaNomes AS
+SELECT  EncarregueArtista.artista as ArtistaID, 
+        Artista.nome as ArtistaNome,
+        EncarregueArtista.funcionario as FuncionarioID,
+        Funcionario.nome as FuncionarioNome
+FROM EncarregueArtista JOIN Artista JOIN Funcionario
+WHERE EncarregueArtista.artista = Artista.id
+    AND EncarregueArtista.funcionario = Funcionario.id;
+
+
+SELECT E1.ArtistaNome as Artista, E1.FuncionarioNome as Funcionario1, E2.FuncionarioNome as Funcionario2
+FROM EncArtistaNomes E1 JOIN EncArtistaNomes E2
+WHERE E1.ArtistaID = E2.ArtistaID
+    AND E1.FuncionarioID < E2.FuncionarioID
+ORDER BY E1.ArtistaNome
