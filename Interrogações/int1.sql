ef08ee16fad3ca86bcc285737a910bceaf2d5cea
Pasgas99 <pasgas99@hotmail.com>
2019-05-26 18:44:11 +0100

    Minor change to query 1

--- Interrogações/int1.sql
+++ Interrogações/int1.sql
@@ -2,10 +2,10 @@
 .headers ON
 .nullvalue NULL
 
-SELECT Concertos.Dia, count(*) as NumPalcos
+SELECT Concertos.Dia as Dia
 FROM
     (SELECT DISTINCT Concerto.data as Dia, Concerto.palco_num as PalcoNum, Concerto.palco_zona as PalcoZona
     FROM Concerto) as Concertos
 GROUP BY Concertos.Dia
-HAVING (NumPalcos > (SELECT count(*)
+HAVING (count(*) = (SELECT count(*)
                     FROM Palco))
